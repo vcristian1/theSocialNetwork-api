407d557fb19a053caaf4782621816d1632871a0b
Cristian Vargas <cristian.v0223@gmail.com>
2022-08-11 15:46:47 -0500

    refactored data.js

--- utils/data.js
+++ utils/data.js
@@ -1,119 +1,208 @@
-const names = [
-    'Aaran',
-    'Aaren',
-    'Aarez',
-    'Aarman',
-    'Aaron',
-    'Aaron-James',
-    'Aarron',
-    'Aaryan',
-    'Aaryn',
-    'Aayan',
-    'Aazaan',
-    'Abaan',
-    'Abbas',
-    'Abdallah',
-    'Abdalroof',
-    'Abdihakim',
-    'Abdirahman',
-    'Abdisalam',
-    'Abdul',
-    'Abdul-Aziz',
-    'Abdulbasir',
-    'Abdulkadir',
-    'Abdulkarem',
-    'Smith',
-    'Jones',
-    'Coollastname',
-    'enter_name_here',
-    'Ze',
-    'Zechariah',
-    'Zeek',
-    'Zeeshan',
-    'Zeid',
-    'Zein',
-    'Zen',
-    'Zendel',
-    'Zenith',
-    'Zennon',
-    'Zeph',
-    'Zerah',
-    'Zhen',
-    'Zhi',
-    'Zhong',
-    'Zhuo',
-    'Zi',
-    'Zidane',
-    'Zijie',
-    'Zinedine',
-    'Zion',
-    'Zishan',
-    'Ziya',
-    'Ziyaan',
-    'Zohaib',
-    'Zohair',
-    'Zoubaeir',
-    'Zubair',
-    'Zubayr',
-    'Zuriel',
-    'Xander',
-    'Jared',
-    'Courtney',
-    'Gillian',
-    'Clark',
-    'Jared',
-    'Grace',
-    'Kelsey',
-    'Tamar',
-    'Alex',
-    'Mark',
-    'Tamar',
-    'Farish',
-    'Sarah',
-    'Nathaniel',
-    'Parker',
-  ];
-  
-  const appDescriptions = [
-    'Decision Tracker',
-    'Find My Phone',
-    'Learn Piano',
-    'Starbase Defender',
-    'Tower Defense',
-    'Monopoly Money Manager',
-    'Movie trailers',
-    'Hello world',
-    'Stupid Social Media App',
-    'Notes',
-    'Messages',
-    'Email',
-    'Compass',
-    'Firefox',
-    'Running app',
-    'Cooking app',
-    'Poker',
-    'Deliveries',
-  ];
-  
-  // Get a random item given an array
-  const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
-  
-  // Gets a random full name
-  const getRandomName = () =>
-    `${getRandomArrItem(names)} ${getRandomArrItem(names)}`;
-  
-  // Function to generate random assignments that we can add to student object.
-  const getRandomAssignments = (int) => {
-    const results = [];
-    for (let i = 0; i < int; i++) {
-      results.push({
-        assignmentName: getRandomArrItem(appDescriptions),
-        score: Math.floor(Math.random() * (99 - 70 + 1) + 70),
-      });
+const firstNames = [
+    'trevor',
+    'tom',
+    'jordan',
+    'fredo',
+    'ella',
+    'milo',
+    'adan',
+    'sophie',
+    'grace',
+    'millie',
+    'mio',
+    'sophia',
+    'carmy',
+    'rosie',
+    'aria',
+    'evie',
+    'josie',
+    'lucy',
+    'mia',
+    'eilidh',
+    'ellie',
+    'lip',
+    'orla',
+    'jaime',
+    'harper',
+    'maya',
+    'willow',
+    'genghis',
+    'leo',
+    'sofia',
+    'mila',
+    'isabella',
+    'ronnie',
+    'hannah',
+    'skye',
+    'lacieniga',
+    'molly',
+    'hallie',
+    'jennifer',
+    'penny',
+    'eva',
+    'esme',
+    'anna',
+    'ariana',
+    'roxie',
+    'callie',
+    'zara',
+    'layla',
+    'emma',
+    'robyn',
+    'chloe',
+    'jack',
+    'noah',
+    'leo',
+    'oliver',
+    'harris',
+    'finlay',
+    'lewis',
+    'james',
+    'rory',
+    'alexander',
+    'brodie',
+    'alfie',
+    'can',
+    'cody',
+    'archie',
+    'lucas',
+    'mason',
+    'finn',
+    'thomas',
+    'freddie',
+    'max',
+    'dimitry',
+    'harry',
+    'jacob',
+    'nevil',
+    'luca',
+    'oscar',
+    'jude',
+    'william',
+    'caleb',
+    'roman',
+    'cameron',
+    'jaxon',
+    'adam',
+    'terry',
+    'jake',
+    'rose',
+    'hannah',
+    'ethan',
+    'harrison',
+    'luke',
+    'arthur',
+    'muhammad',
+    'jamie',
+    'liam',
+    'reuben',
+    'arlo',
+    'grayson',
+    'hunter',
+    'kai',
+    'aaron'
+];
+const lastNames = [
+    'elsher',
+    'solace',
+    'levine',
+    'thatcher',
+    'raven',
+    'vargas',
+    'james',
+    'johnson',
+    'cromwell',
+    'ashley',
+    'davis',
+    'west',
+    'langley',
+    'daughtler',
+    'madison',
+    'marley',
+    'ellis',
+    'hope',
+    'cassidy',
+    'lopez',
+    'jenkins',
+    'poverly',
+    'mckenna',
+    'gonzales',
+    'keller',
+    'collymore',
+    'stoll',
+    'miller',
+    'adler',
+    'huxley',
+    'ledger',
+    'hayes',
+    'ford',
+    'finnegan',
+    'beckett',
+    'gatlin',
+    'dick',
+    'zimmerman',
+    'jacobs',
+    'wilson',
+    'heffernan',
+    'gray',
+    'curran',
+    'crassus',
+    'anderson',
+    'adams',
+    'gay',
+    'hendrix',
+    'lennon',
+    'casper'
+];
+const emails = [
+    '@gmail.com',
+    '@yahoo.com',
+    '@hotmail.com',
+    '@outlook.com',
+    '@live.com',
+    '@icloud.com',
+    '@aol.com',
+    '@firstlookappraisals.com',
+    '@industria.com'
+];
+
+const thoughts = [
+    ' hates when you drop your ice cream!',
+    ' never went to see the live action Lion King.',
+    ' wonders what its like to be a billionairre.',
+    ' reada a book twice a day',
+    " doesn't like to waste their time, or anyone else's time!",
+    ' was catfished when they met their online date at the club!',
+    ' has a pet cricket named Jimmy.',
+    ' wants to be a web developer!'
+];
+
+// Get a random item given an array
+const getRandomArrItem = (arr) => arr[Math.floor(Math.random() * arr.length)];
+
+const getRandomUser = () => {
+    const fName = getRandomArrItem(firstNames);
+    const lName = getRandomArrItem(lastNames);
+    const uName = `${fName}.${lName}`
+    const email = `${uName}${getRandomArrItem(emails)}`;
+    return { uName, email };
+}
+
+const getRandomThoughts = (user) => {
+    const rand = Math.floor(Math.random() * 3);
+    let result = [];
+    for(let i=0; i<rand; i++){
+        const end = getRandomArrItem(thoughts);
+        const name = getRandomArrItem(firstNames);
+        result.push(
+            {
+                thoughtText: `${name}${end}`,
+                username: `${user.username}`,
+                email: `${user.email}`
+            }
+        )
     }
-    return results;
-  };
-  
-  // Export the functions for use in seed.js
-  module.exports = { getRandomName, getRandomAssignments };
-  
+    return result;
+}
+
+module.exports = { getRandomUser, getRandomThoughts };
