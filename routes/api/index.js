164ec997bcde6e3c535b7b94f8e758fc43525729
Cristian Vargas <cristian.v0223@gmail.com>
2022-08-11 13:13:56 -0500

    removed friendRoutes from index.js within api sub directory in routes directory

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const thoughtRoutes = require('./thoughtsRoutes');
-const friendRoutes = require('.friendsRoutes');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
-router.use('/friend', friendRoutes);
 
 module.exports = router;
