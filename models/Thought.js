28b153e09a56cfeff8e85ff27f304d72b93da2d8
Cristian Vargas <cristian.v0223@gmail.com>
2022-08-17 19:12:24 -0500

    refactored code to show thoughts correctly in insomnia

--- models/Thought.js
+++ models/Thought.js
@@ -23,28 +23,32 @@ const reactionSchema = new Schema(
   }
 );
 
-// Schema to create Thought model
 const thoughtSchema = new Schema(
   {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (formatDate) =>
+        `${formatDate.toLocaleDateString()} ${formatDate.toLocaleTimeString()}`,
+    },
     username: {
       type: String,
       required: true,
     },
-    thoughtText: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    createdAt: {
-        type: Date,
-        default: Date.now,
-    },
-    reactions: [ reactionSchema ],
+    reactions: [reactionSchema],
   },
   {
     toJSON: {
       virtuals: true,
+      getters: true,
     },
+    id: false,
   }
 );
 
@@ -56,6 +60,6 @@ thoughtSchema
   })
 
 
-const Thought = model('thought', thoughtSchema);
+const Thought = model('Thought', thoughtSchema);
 
 module.exports = Thought;
--- models/User.js
+++ models/User.js
@@ -50,6 +50,6 @@ userSchema.virtual('friendCount').get(function() {
     return `${this.friends.length}`;
 });
 
-const User = model('user', userSchema);
+const User = model('User', userSchema);
 
 module.exports = User;
